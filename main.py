b305016beba4133c141b6ccf379ed0fd67534f83
Leonid <legeorg2004@gmail.com>
2020-12-21 09:08:16 +0500

    в этом файле создаю карту всей игры необычным образом

--- main.py
+++ main.py
@@ -1,51 +1,76 @@
 import pygame
 from setting import *
-from math import cos, sin, ceil
-from map import *
+from player import *
+import os, sys
+import math
+from map import world_map
+from ray_casting import ray_casting
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
 
 pygame.init()
-screen = pygame.display.set_mode((WIDTH_SCREEN, HIGHT_SCREEN))
-run = True
+all_sprites = pygame.sprite.Group()
+sc = pygame.display.set_mode((WIDTH, HEIGHT))
+
+class Pol(pygame.sprite.Sprite):
+    image_pol = load_image('pol_2.png')
+
+    def __init__(self, *group):
+        super().__init__(*group)
+        self.image = self.image_pol
+        self.rect = self.image.get_rect()
+        self.rect.y = HEIGHT // 2
+        self.perem = 10
+
+    def update(self):
+        pass
+        # pygame.time.delay(200)
+        # self.perem = -self.perem
+        # self.rect.y -= self.perem
+
+sprite = Pol()
+sprite.image = pygame.transform.scale(load_image('pol_2.png'), (WIDTH, HEIGHT // 2))
+sprite.rect = sprite.image.get_rect()
+sprite.rect.y = HEIGHT // 2
+
 clock = pygame.time.Clock()
-angle = 0
-player = pygame.draw.circle(screen, 'green', (250, 250), 20)
+player = Player()
+all_sprites.add(sprite)
 
-while run:
-    screen.fill('black')
-    KEYS = pygame.key.get_pressed()
 
-    for x1, y1 in world_map:
-        pygame.draw.rect(screen, 'gray', (x1, y1, TILE_X, TILE_Y), 3)
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+    player.movement()
+    all_sprites.update()
+    sc.fill(WHITE)
 
-    for i in pygame.event.get():
-        if i.type == pygame.QUIT:
-            run = False
+    pygame.draw.rect(sc, BLACK, (0, 0, WIDTH, HALF_HEIGHT))
+    all_sprites.draw(sc)
 
-    cos_a = cos(angle)
-    sin_a = sin(angle)
-    if KEYS[pygame.K_w]:
-        player.x += PLAYER_STEP * cos_a
-        player.y += PLAYER_STEP * sin_a
-    if KEYS[pygame.K_a]:
-        player.y += -PLAYER_STEP * cos_a
-        player.x += PLAYER_STEP * sin_a
-    if KEYS[pygame.K_d]:
-        player.y += PLAYER_STEP * cos_a
-        player.x += -PLAYER_STEP * sin_a
-    if KEYS[pygame.K_s]:
-        player.x += -PLAYER_STEP * cos_a
-        player.y += -PLAYER_STEP * sin_a
-    if KEYS[pygame.K_LEFT]:
-        angle -= ANGLE_SPEED
-    if KEYS[pygame.K_RIGHT]:
-        angle += ANGLE_SPEED
-    if KEYS[pygame.K_SPACE]:
-        print(angle, cos_a, sin_a)
-    pygame.draw.circle(screen, 'tomato', (player.x, player.y), 20)
-    for i in range(-ANGLE, ANGLE + 1):
-        i /= 100
-        pygame.draw.line(screen, 'gold', (player.x, player.y),
-                         (player.x + OBZOR * cos(angle + i), player.y + OBZOR * sin(angle + i)), 1)
+    ray_casting(sc, player.pos, player.angle)
+
+    # pygame.draw.circle(sc, GREEN, (int(player.x), int(player.y)), 12)
+    # pygame.draw.line(sc, GREEN, player.pos, (player.x + WIDTH * math.cos(player.angle),
+    #                                          player.y + WIDTH * math. sin(player.angle)), 2)
+    # for x,y in world_map:
+    #     pygame.draw.rect(sc, DARKGRAY, (x, y, TILE, TILE), 2)
 
     pygame.display.flip()
     clock.tick(FPS)
+    # print(clock.get_fps())
--- map.py
+++ map.py
@@ -1,20 +1,18 @@
 from setting import *
-MAP = [
-    'QQQQQQQQQQ',
-    'Q........Q',
-    'Q........Q',
-    'Q..QQQQ..Q',
-    'Q..QQqQ..Q',
-    'Q..QQQQ..Q',
-    'Q........Q',
-    'Q........Q',
-    'Q........Q',
-    'QQQQQQQQQQ'
+
+text_map = [
+    'WWWWWWWWWWWW',
+    'W......W...W',
+    'W..WWW...W.W',
+    'W....W..WW.W',
+    'W..W....W..W',
+    'W..W...WWW.W',
+    'W....W.....W',
+    'WWWWWWWWWWWW'
 ]
+
 world_map = set()
-TILE_X, TILE_Y = WIDTH_SCREEN // len(MAP[0]), HIGHT_SCREEN // len(MAP)
-for i, row in enumerate(MAP):
-    for j, char in enumerate(row):
-        if char.upper() == 'Q':
-            world_map.add((j * TILE_X, i * TILE_Y))
-world_map = world_map.copy()
+for j, row in enumerate(text_map):
+    for i, char in enumerate(row):
+        if char == 'W':
+            world_map.add((i * TILE, j * TILE))
