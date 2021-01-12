8960ff59f9f47cda4561ecb5445140d8031edd1c
georg.le <legeorg2004@gmail.com>
2021-01-12 17:09:10 +0500

    добавлен прыжок

--- new_phis.py
+++ new_phis.py
@@ -13,15 +13,29 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__(all_sprites, player_group)
         self.image = player
+        self.count_jump = 10
+        self.jumping = False
         self.rect = self.image.get_rect()
 
     def update(self):
-        if not pygame.sprite.spritecollideany(self, map_group):
-            self.rect.y += 1
-        if KEYS[pygame.K_d]:
+        if not pygame.sprite.spritecollideany(self, map_group) and not self.jumping:
+            self.rect.y += 5
+        if KEYS[pygame.K_SPACE] and not self.jumping:
+            self.jumping = True
+        if KEYS[pygame.K_d] and self.rect.x <= screen.get_width():
             self.rect.x += 5
-        if KEYS[pygame.K_a]:
+        if KEYS[pygame.K_a] and self.rect.x >= 0:
             self.rect.x -= 5
+        if self.jumping:
+            if self.count_jump >= -10:
+                if self.count_jump < 0:
+                    self.rect.y += (self.count_jump ** 2)
+                else:
+                    self.rect.y -= (self.count_jump ** 2)
+                self.count_jump -= 1
+            else:
+                self.jumping = False
+                self.count_jump = 10
 
 
 # class Not_Static_Object():
@@ -127,4 +141,4 @@ while True:
     map_group.draw(screen)
 
     pygame.display.flip()
-    clock.tick(100)
+    clock.tick(60)
