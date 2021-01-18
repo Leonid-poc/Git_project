322aa0dae9300d709896706edd5c2b34201b2d9a
Roman <Tezer2k172yandex.ru>
2021-01-18 22:12:44 +0500

    Добавил нормальную физику для снарядров, НОРМАЛЬНО переопределил группы СПРАЙТОВ, сделал возможность год мода на T + I + O, сделал полоску ХП, перебрал еще основной цикл, для более адекватной загрузки и отображения всех спрайтов, если что, поставил комментарии

--- new_phis.py
+++ new_phis.py
@@ -5,67 +5,126 @@ import pygame
 pygame.init()
 clock = pygame.time.Clock()
 
+
 # класс игрока который отвечает за любые события и изменения персонажа
 class Player(pygame.sprite.Sprite):
+    left_pers = False
+    right_pers = True
+
     def __init__(self):
         super(Player, self).__init__(all_sprites, player_group)
         self.image = pers
+        self.mask = pygame.mask.from_surface(self.image)
         self.count_jump = 20
         self.jumping = False
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = 0, 735
+        self.hp = 100
+        self.god_mode = False
 
     def return_now_skin(self):
         return self.image
 
+    def give_mod(self):
+        if not self.god_mode:
+            self.god_mode = True
+        else:
+            self.god_mode = False
+
+    def proof_pos(self):
+        for i in map_coords_spisok:
+            if self.rect.x + self.rect.width > i[0] and self.rect.y + self.rect.height > i[1]:
+                return False
+            if i[0] + i[2] > self.rect.x and i[1] + i[3] > self.rect.y:
+                return False
+            return True
+
+    def povorot_pers(self):
+        return (self.left_pers, self.right_pers)
+
+    def proof_font_fall_out_map(self):
+        for i in map_coords_spisok:
+            if self.rect.topright[0] >= i[0] and self.rect.x <= i[0] + i[2] and i[1] - 180 <= self.rect.y <= i[1]:
+                print(self.rect.bottom, i[1])
+                while self.rect.bottom > i[1]:
+                    self.rect.y -= 1
+
+    def send_hp(self):
+        if self.god_mode:
+            pass
+        return FONT.render(f'{self.hp}', False, (255, 0, 0))
+
+
+
     def update(self, image):
         if self.image != image:
+            x, y = self.rect.x, self.rect.y
             self.image = image
             self.rect = self.image.get_rect()
-            self.rect.x, self.rect.y = 0, 745
-        if not pygame.sprite.spritecollideany(self, map_group) and not self.jumping:
+            self.rect.x, self.rect.y = x, y
+            print(self.rect.y)
+        if not pygame.sprite.spritecollide(self, map_group, False, pygame.sprite.collide_mask) and not self.jumping:
             self.rect.y += 5
+
         if KEYS[pygame.K_SPACE] and not self.jumping and pygame.sprite.spritecollideany(self, map_group):
             self.jumping = True
+
         if KEYS[pygame.K_d] and self.rect.x + self.rect.width <= screen.get_width():
+            self.right_pers, self.left_pers = True, False
             self.rect.x += 5
+
         if KEYS[pygame.K_a] and self.rect.x >= 0:
+            self.right_pers, self.left_pers = False, True
             self.rect.x -= 5
+
+        if KEYS[pygame.K_q]:
+            if not self.god_mode:
+                Projectale(self.rect, True)
+            else:
+                Projectale(self.rect)
+
         if self.jumping:
             if self.count_jump >= -20:
                 if self.count_jump < 0:
-                    if not pygame.sprite.spritecollideany(self, map_group):
+                    if not pygame.sprite.spritecollide(self, map_group, False, pygame.sprite.collide_mask):
                         self.rect.y += (self.count_jump ** 2) / 10
                     else:
                         self.jumping = False
                         self.count_jump = 20
+                        self.proof_font_fall_out_map()
                 else:
                     self.rect.y -= (self.count_jump ** 2) / 10
                 self.count_jump -= 1
             else:
-                if not pygame.sprite.spritecollideany(self, map_group):
+                if not pygame.sprite.spritecollide(self, map_group, False, pygame.sprite.collide_mask):
                     self.rect.y += (self.count_jump ** 2) / 10
                     self.count_jump -= 1
                 else:
                     self.jumping = False
                     self.count_jump = 20
+                    self.proof_font_fall_out_map()
 
 
 class Projectale(pygame.sprite.Sprite):
-    def __init__(self, person):
-        super(Projectale, self).__init__(all_sprites, projectales)
-        self.pers_pos_x, self.pers_pos_y = person.get_rect()
-        self.image = pygame.transform.scale(load_image(r'Other\ball.png'), (60, 40))
+    def __init__(self, person, god_mode=False):
+        super().__init__(projectales)
+        self.mode = god_mode
+        self.pers_pos_x, self.pers_pos_y = person.x, person.y
+        if self.mode:
+            self.image = pygame.transform.scale(load_image(r'Other\qt.png'), (100, 60))
+        else:
+            self.image = pygame.transform.scale(load_image(r'Other\qt.png'), (200, 120))
         self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = self.pers_pos_x, self.pers_pos_y
 
-
-    def shot(self):
-        # self.pos = self.image.get_rect()
-        self.rect.x += 10
-
-        projectales.update(self.image)
-
-
+    def update(self):
+        if not (pygame.sprite.spritecollide(self, map_group, True) or self.rect.topright[0] >= screen.get_width()):
+            if not self.mode:
+                self.rect.x += 100
+            else:
+                self.rect.x += 40
+        else:
+            self.kill()
 
 # вызываю определённые классы которые автоматически отрисовывваются
 Shop()
@@ -73,26 +132,33 @@ Settings()
 Player = Player()
 draw_map()
 
-
 while True:
     # Основной цикл, куда уж без него, если ты читал комментарии до этого, ты должен всё понять
     # Есть баг с нажатием пробела (110, 112 строчки), помоги исправить, плез, перс улетает в потолок
-    KEYS = pygame.key.get_pressed()
+
     screen.blit(return_background(), (0, 0))
+    KEYS = pygame.key.get_pressed()
     for i in pygame.event.get():
         if i.type == pygame.MOUSEBUTTONDOWN:
             sprites_dop.update(i.pos)
         if i.type == pygame.QUIT or KEYS[pygame.K_F10]:
             sys.exit()
-    if Player.return_now_skin() != return_skin():
-        player_group.update(return_skin())
-    player_group.update(Player.return_now_skin())
+        if KEYS[pygame.K_t] + KEYS[pygame.K_i] + KEYS[pygame.K_o]:
+            Player.give_mod()
+        # if i.type ==
+    # if Player.return_now_skin() not in (return_skin()):
+    player_group.update(return_skin())
+    # player_group.update(Player.return_now_skin())
 
-    ren_font = FONT.render(f'{int(clock.get_fps())}', True, (255, 255, 255))
-    screen.blit(ren_font, (0, 0))
-    Projectale.shot(Projectale)
+    ren_fon = FONT.render(f"{int(clock.get_fps())}", True, (255, 255, 255))
+    screen.blit(ren_fon, (0, 0))
+    # Отрисовка спрайтов
     all_sprites.draw(screen)
-    map_group.draw(screen)
+    projectales.update()
     projectales.draw(screen)
+    map_group.draw(screen)
+    # Отрисовка кол-ва хп
+    screen.blit(Player.send_hp(), (150, 0))
+    # Смена кадра
     pygame.display.flip()
     clock.tick(FPS)
