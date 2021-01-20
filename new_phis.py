d7beee1e60a57fd44b11a500b8f4277a51378a5d
Roman <Tezer2k172yandex.ru>
2021-01-20 14:01:20 +0500

    Добавил анимированные полоски хп и маны, теперь стрельба расходует ману, а она пополняется со временем

--- new_phis.py
+++ new_phis.py
@@ -8,9 +8,6 @@ clock = pygame.time.Clock()
 
 # класс игрока который отвечает за любые события и изменения персонажа
 class Player(pygame.sprite.Sprite):
-    left_pers = False
-    right_pers = True
-
     def __init__(self):
         super(Player, self).__init__(all_sprites, player_group)
         self.image = pers
@@ -19,8 +16,12 @@ class Player(pygame.sprite.Sprite):
         self.jumping = False
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = 0, 735
-        self.hp = 100
+        self.START_HP = 180
+        self.NOW_HP = 180
+        self.START_MANA = 200
+        self.NOW_MANA = 200
         self.god_mode = False
+        self.time_to_restart_mana = 0
 
     def return_now_skin(self):
         return self.image
@@ -39,9 +40,6 @@ class Player(pygame.sprite.Sprite):
                 return False
             return True
 
-    def povorot_pers(self):
-        return (self.left_pers, self.right_pers)
-
     def proof_font_fall_out_map(self):
         for i in map_coords_spisok:
             if self.rect.topright[0] >= i[0] and self.rect.x <= i[0] + i[2] and i[1] - 180 <= self.rect.y <= i[1]:
@@ -52,9 +50,15 @@ class Player(pygame.sprite.Sprite):
     def send_hp(self):
         if self.god_mode:
             pass
-        return FONT.render(f'{self.hp}', False, (255, 0, 0))
+        return self.START_HP
 
 
+    def up_mana(self):
+        if self.NOW_MANA != self.START_MANA:
+            self.time_to_restart_mana += 1
+            if self.time_to_restart_mana == 100:
+                self.time_to_restart_mana = 0
+                self.NOW_MANA += 20
 
     def update(self, image):
         if self.image != image:
@@ -78,10 +82,12 @@ class Player(pygame.sprite.Sprite):
             self.rect.x -= 5
 
         if KEYS[pygame.K_q]:
-            if not self.god_mode:
-                Projectale(self.rect, True)
-            else:
-                Projectale(self.rect)
+            if self.NOW_MANA >= 20:
+                self.NOW_MANA -= 20
+                if not self.god_mode:
+                    Projectale(self, self.rect, True)
+                else:
+                    Projectale(self, self.rect)
 
         if self.jumping:
             if self.count_jump >= -20:
@@ -106,10 +112,14 @@ class Player(pygame.sprite.Sprite):
 
 
 class Projectale(pygame.sprite.Sprite):
-    def __init__(self, person, god_mode=False):
+    def __init__(self, person, person_rect, god_mode=False):
         super().__init__(projectales)
         self.mode = god_mode
-        self.pers_pos_x, self.pers_pos_y = person.x, person.y
+        self.person_rect = person_rect
+        self.person = person
+        self.pers_pos_x, self.pers_pos_y = person_rect.x, person_rect.y
+
+
         if self.mode:
             self.image = pygame.transform.scale(load_image(r'Other\qt.png'), (100, 60))
         else:
@@ -118,18 +128,34 @@ class Projectale(pygame.sprite.Sprite):
         self.rect.x, self.rect.y = self.pers_pos_x, self.pers_pos_y
 
     def update(self):
-        if not (pygame.sprite.spritecollide(self, map_group, True) or self.rect.topright[0] >= screen.get_width()):
-            if not self.mode:
-                self.rect.x += 100
+
+            if not (pygame.sprite.spritecollide(self, map_group, True) or self.rect.topright[0] >= screen.get_width()):
+                if not self.mode:
+                    self.rect.x += 100
+                else:
+                    self.rect.x += 40
             else:
-                self.rect.x += 40
-        else:
-            self.kill()
+                self.kill()
+
+
+class Indicator:
+    def __init__(self, stat, start_stat, color, x, y):
+        self.stat = stat
+        self.start_stat = start_stat
+        self.hp_procent = int((self.stat / self.start_stat) * 100) / 100
+        self.COLOR_TEXT= (255, 255, 255)
+        self.COLOR_PAN = color
+        self.x, self.y = x, y
+        self.FOOT, self.ROS = 140, 65
+    def show(self):
+        pygame.draw.rect(screen, (self.COLOR_PAN), (self.x, self.y, int(self.FOOT * self.hp_procent), self.ROS))
+        pygame.draw.rect(screen, (0,0,0), (self.x, self.y, self.FOOT, self.ROS), 5)
+        screen.blit(FONT.render(f'{self.stat}', True, self.COLOR_TEXT), (self.x+15, 5))
 
 # вызываю определённые классы которые автоматически отрисовывваются
 Shop()
 Settings()
-Player = Player()
+Player1 = Player()
 draw_map()
 
 while True:
@@ -144,12 +170,12 @@ while True:
         if i.type == pygame.QUIT or KEYS[pygame.K_F10]:
             sys.exit()
         if KEYS[pygame.K_t] + KEYS[pygame.K_i] + KEYS[pygame.K_o]:
-            Player.give_mod()
+            Player1.give_mod()
         # if i.type ==
     # if Player.return_now_skin() not in (return_skin()):
     player_group.update(return_skin())
     # player_group.update(Player.return_now_skin())
-
+    Player1.up_mana()
     ren_fon = FONT.render(f"{int(clock.get_fps())}", True, (255, 255, 255))
     screen.blit(ren_fon, (0, 0))
     # Отрисовка спрайтов
@@ -158,7 +184,11 @@ while True:
     projectales.draw(screen)
     map_group.draw(screen)
     # Отрисовка кол-ва хп
-    screen.blit(Player.send_hp(), (150, 0))
+
+    ind_hp = Indicator(Player1.NOW_HP, Player1.START_HP, (255, 0, 0), 100, 0)
+    ind_hp.show()
+    ind_mana = Indicator(Player1.NOW_MANA, Player1.START_MANA, (0, 0, 255), 260, 0)
+    ind_mana.show()
     # Смена кадра
     pygame.display.flip()
     clock.tick(FPS)
